ac233ceeb3dc10fa8c97cbf5bd6fd1849d04d34e
ryo0223 <ryo0221@outlook.com>
2019-07-13 13:30:11 +0100

    settings.py changed

--- knn/settings.py
+++ knn/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+de5j!&9-h+3k$y21jr*v0c9-$07%l+9!b_@qd=m@d!-1per%0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["knnapi.herokuapp.com","127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
--- knnapp/views.py
+++ knnapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 from rest_framework.views import APIView
 from rest_framework.parsers import MultiPartParser, FormParser
@@ -8,6 +8,7 @@ from rest_framework import status,serializers
 from .serialisers import FileSerializer
 import pandas as pd
 import os
+import knnapp.knn_algorithm
 class IncredibleInputSerializer(serializers.Serializer):
     model_input = serializers.FileField()
 class FileView(APIView):
@@ -22,6 +23,9 @@ class FileView(APIView):
       csv_data=pd.read_csv("./media/"+str(file_obj))
       print(csv_data.head())
       os.remove("./media/"+str(file_obj))
-      return Response(file_serializer.data, status=status.HTTP_201_CREATED)
+      knnapp.knn_algorithm.knn_algo()
+      #return Response(file_serializer.data, status=status.HTTP_201_CREATED)
+
+      return HttpResponse("success", content_type="text/plain")
     else:
       return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
